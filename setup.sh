a403b21736f27f0bf8a388f8bc83e546a1ecd9e1
rochaedu1722 <rochaedu@icloud.com>
2025-06-11 23:00:38 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🔧 Instalando dependências..."
-pip install -r requirements.txt
-
-echo "🔧 Ajustando banco de dados..."
-python ajustar_banco.py
-
-echo "🚀 Iniciando bot Rochinha Pé Duro com scheduler..."
-python bot.py
